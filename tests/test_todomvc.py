83fb73ba420753a6acabef939b9249b3eb65f9fc
maxellius <58748757+maxellius@users.noreply.github.com>
2021-02-07 02:38:57 +0300

    first commit

---
+++ tests/test_todomvc.py
@@ -0,0 +1,27 @@
+from selene import have
+from selene.support.shared import browser
+
+
+def test_common_tasks_management():
+    browser.open('https://todomvc4tasj.herokuapp.com/')
+    browser.should(have.js_returned(True, "return $._data($('#clear-completed')[0], 'events').hasOwnProperty('click')"))
+
+    browser.element('#new-todo').type('a').press_enter()
+    browser.element('#new-todo').type('b').press_enter()
+    browser.element('#new-todo').type('c').press_enter()
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
+
+    browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
+    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_enter()
+
+    browser.all('#todo-list>li').element_by(have.exact_text('b edited')).element('.toggle').click()
+
+    browser.element('#clear-completed').click()
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'c'))
+
+    browser.all('#todo-list>li').element_by(have.exact_text('c')).double_click()
+    browser.all('#todo-list>li').element_by(have.css_class('editing')) \
+        .element('.edit').type(' to be canceled').press_escape()
+
+    browser.all('#todo-list>li').element_by(have.exact_text('b')).hover().element('.destroy').click()
+    browser.all('#todo-list>li').should(have.exact_texts('a'))
