b6956260a4687a34bea118245c97078de50b4d6f
maxellius <58748757+maxellius@users.noreply.github.com>
2021-02-08 19:49:06 +0300

    Update test_todomvc.py

--- tests/test_todomvc.py
+++ tests/test_todomvc.py
@@ -12,16 +12,19 @@ def test_common_tasks_management():
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
     browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
-    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_enter()
+    browser.all('#todo-list>li').element_by(have.css_class('editing'))\
+        .element('.edit').type(' edited').press_enter()
 
-    browser.all('#todo-list>li').element_by(have.exact_text('b edited')).element('.toggle').click()
+    browser.all('#todo-list>li').element_by(have.exact_text('b edited'))\
+        .element('.toggle').click()
 
     browser.element('#clear-completed').click()
     browser.all('#todo-list>li').should(have.exact_texts('a', 'c'))
 
     browser.all('#todo-list>li').element_by(have.exact_text('c')).double_click()
-    browser.all('#todo-list>li').element_by(have.css_class('editing')) \
+    browser.all('#todo-list>li').element_by(have.css_class('editing'))\
         .element('.edit').type(' to be canceled').press_escape()
 
-    browser.all('#todo-list>li').element_by(have.exact_text('b')).hover().element('.destroy').click()
+    browser.all('#todo-list>li').element_by(have.exact_text('c')).hover()\
+        .element('.destroy').click()
     browser.all('#todo-list>li').should(have.exact_texts('a'))
