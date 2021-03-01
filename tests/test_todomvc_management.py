77bd445e18395c96a21a07e9494d92961b51222d
maxellius <58748757+maxellius@users.noreply.github.com>
2021-03-01 23:42:49 +0300

    corrections

--- tests/test_todomvc_management.py
+++ tests/test_todomvc_management.py
@@ -1,4 +1,4 @@
-from todomvc_test.modul import todos
+from todomvc_test.model import todos
 
 
 def test_todos_life_cycle():
@@ -7,15 +7,13 @@ def test_todos_life_cycle():
     todos.add('a', 'b', 'c')
     todos.should_be('a', 'b', 'c')
 
-    todos.edit('b', ' edited')
+    todos.edit('b', 'b edited')
     todos.toggle('b edited')
 
     todos.clear_completed()
     todos.should_be('a', 'c')
 
-    todos.cancel_edit('c', ' to be canceled')
+    todos.cancel_edit('c', 'c to be canceled')
 
     todos.delete('c')
     todos.should_be('a')
-
-
--- todomvc_test/model/todos.py
+++ todomvc_test/model/todos.py
@@ -1,8 +1,7 @@
 from selene.support.conditions import have
 from selene.support.shared import browser
 
-todos = browser.all('#todo-list>li')
-new_todo = browser.element('#new-todo')
+todo_list = browser.all('#todo-list>li')
 is_js_loaded = ("return $._data($('#clear-completed')[0], "
                      "'events').hasOwnProperty('click')")
 
@@ -13,31 +12,31 @@ def visit():
     browser.should(have.js_returned(True, is_js_loaded))
 
 
-def add(*args: str):
-    for text in args:
-        new_todo.type(text).press_enter()
+def add(*text: str):
+    for text in text:
+        browser.element('#new-todo').type(text).press_enter()
 
 
 def should_be(*text: str):
-    todos.should(have.exact_texts(*text))
+    todo_list.should(have.exact_texts(*text))
 
 
-def start_editing(todo: str, adding_text: str):
-    todos.element_by(have.exact_text(todo)).double_click()
-    return todos.element_by(have.css_class('editing')) \
-        .element('.edit').type(adding_text)
+def start_editing(old_todo: str, corrected_todo: str):
+    todo_list.element_by(have.exact_text(old_todo)).double_click()
+    return todo_list.element_by(have.css_class('editing')) \
+        .element('.edit').with_(set_value_by_js=True).set_value(corrected_todo)
 
 
-def edit(todo: str, adding_text: str):
-    start_editing(todo, adding_text).press_enter()
+def edit(old_todo: str, corrected_todo: str):
+    start_editing(old_todo, corrected_todo).press_enter()
 
 
-def cancel_edit(todo: str, adding_text: str):
-    start_editing(todo, adding_text).press_escape()
+def cancel_edit(old_todo: str, corrected_todo: str):
+    start_editing(old_todo, corrected_todo).press_escape()
 
 
 def toggle(todo: str):
-    todos.element_by(have.exact_text(todo)) \
+    todo_list.element_by(have.exact_text(todo)) \
         .element('.toggle').click()
 
 
@@ -46,5 +45,5 @@ def clear_completed():
 
 
 def delete(text: str):
-    todos.element_by(have.exact_text(text)).hover() \
+    todo_list.element_by(have.exact_text(text)).hover() \
         .element('.destroy').click()
